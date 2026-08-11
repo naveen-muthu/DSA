93464349309d833038c3311a6d728796f7d620f4
Naveen-m <naveenking8171@gmail.com>
2026-08-11 19:44:24 +0530

    Time: 3 ms (13.92%) | Memory: 43.3 MB (77.99%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int j=0;j<nums.length-1;j++){
+        for(int i=0;i<nums.length-1-j;i++){
+            if(nums[i]>nums[i+1]){
+                int tep=nums[i];
+                nums[i]=nums[i+1];
+                nums[i+1]=tep;
+            }
+        }
+        }
+
+        
+    }
+}
